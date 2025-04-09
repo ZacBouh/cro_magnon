ddba5d1ba010ee9eadb9024682afeba59bba2e44
ZacBouh <zacharie.bouhaya@gmail.com>
2025-04-09 16:46:50 +0000

    backend basic CRUD completed

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import express from 'express';
 import multer from 'multer';
 import path from 'path';
-import fs from 'fs';
-import { savePost, getPosts } from './storage.js' 
+import { savePost, getPosts, getPostById, deletePost } from './storage.js' 
 
 const app = express();
 const port = 80;
@@ -23,26 +22,46 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 // Routes
-app.get('/', (req, res) => {
-  res.render('list-article', {articles: null});
+app.get('/', async (req, res) => {
+  const articles = await getPosts()
+  console.log('Response articles : ')
+  console.dir(articles)
+  res.render('list-article', {articles});
 });
 
 app.get('/create', (req, res) => {
   res.render('create-form');
 });
 
-app.get('/update', (req, res) => {
-  res.render('update-form', {});
+app.get('/articles/:id/edit', async (req, res) => {
+  const postId = req.params.id
+  if (!postId) res.redirect('/')
+  
+  const article = await getPostById(postId)
+  if (!article) res.redirect('/')
+
+  res.render('update-form', {article});
 });
 
+app.post('/articles/:id', async (req,res) => {
+  const postId = req.params.id
+  if (!postId) res.redirect('/')
+  const method = req.query._method 
 
-app.post('/submit', upload.single('fichier'), (req, res) => {
-  
+  if (method === 'DELETE'){
+    const result = await deletePost(postId)
+  }
+  res.redirect('/')
+})
+
+app.post('/submit', upload.single('fichier'), async (req, res) => {
+  let articles = null
+
   try {
-
+    req.body['img'] = req.file.filename
     console.log('Saving post : ', req.body)
-    savePost(req.body)
-    articles = getPosts()
+    await savePost(req.body)
+    articles = await getPosts()
   } catch(err) {
     console.log('Failed to save post : ', err)
     res.render('list-article')
--- storage.js
+++ storage.js
@@ -1,8 +1,8 @@
 import { randomUUID } from 'crypto';
-import fs from 'fs';
+import fs from 'fs/promises';
 
-export const savePost = (post) => {
-    let posts = getPosts()
+export const savePost = async  (post) => {
+    let posts = await getPosts()
 
     if (!posts){
         posts = []
@@ -11,32 +11,62 @@ export const savePost = (post) => {
     posts.push(post)
     
     const data = JSON.stringify(posts)
-    fs.writeFile('posts.json', data, (err) => {
+    await fs.writeFile('posts.json', data, (err) => {
         console.error(`failed to save post : \n${err}`)
     })
-    return 
+    return post
 }
 
-export const getPosts = () => {
+export const getPosts = async () => {
     let posts = null
-    fs.readFile('posts.json', 'utf8', (err, rawPosts)=>{
-        if(err){
-            console.error(`Error retrieving posts : \n${err}`)
-        }
-        try {
-            posts = JSON.parse(rawPosts)
-        } catch (err) {
-            console.error(`Error parsing saved posts : \n${err}`)
-        }       
-    })
-
+    console.log('Retrieving stored posts')
+    try {
+        const rawData = await fs.readFile('posts.json', 'utf8')
+        posts = JSON.parse(rawData)
+    } catch (err) {
+        console.log('Error reading post data from file : \n', err)
+    }
+    console.log('Retrieved saved posts data : \n', posts)
     return posts
 }
 
-const deletePost = (postId) => {
+export const deletePost = async (postId) => {
+    try {
+        const posts = await getPosts()
+        if (!posts) {
+            console.log('Error: no saved post found')
+            return false
+        }
+        const post = await getPostById(postId)
+        if (!post) {
+            console.log('No file with ID ', postId, ' to delete')
+            return false
+        }
+
+        const newPostsState = posts.filter((post) => post.id != postId)
+        await fs.writeFile('posts.json', JSON.stringify(newPostsState),'utf8')
+        await fs.unlink(`./fichiers/${post.img}`)
+
+        return true
+        
+    } catch (err) {
+        console.log('Error trying to delete post : ', err)
+        return false
+    }
 
 }
 
-const getPostById = (postId) => {
+export const getPostById = async (postId) => {
+    const posts = await getPosts()
+    if (!posts) {
+        console.log('Error: no saved post found')
+        return null
+    }
 
+    const post = posts.filter((post) => post.id == postId)[0]
+    if (!post) {
+        console.log('No post found with ID ', postId)
+        return null
+    }
+    return post 
 }
